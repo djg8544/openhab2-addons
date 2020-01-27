eb9116e25a5cadae10f6dab0ed5a3b553eaaff8f
Dustin Gerold <d_gerold_99@yahoo.com>
2020-01-27 10:44:55 -0600

    general exception handling

--- addons/binding/org.openhab.binding.upb/src/main/java/org/openhab/binding/upb/handler/LinkHandler.java
+++ addons/binding/org.openhab.binding.upb/src/main/java/org/openhab/binding/upb/handler/LinkHandler.java
@@ -12,16 +12,19 @@ import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.types.State;
 import org.openhab.binding.upb.UPBBindingConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Handles commands sent to a {@link UPBBindingConstants#THING_TYPE_LINK}.
  *
- * @author Chris Van Orman
+ * @author Chris Van Orman, Dustin Gerold
  * @since 2.2.0
  *
  */
-public class LinkHandler extends UPBBaseHandler {
-
+public class LinkHandler extends UPBBaseHandler 
+{
+    private final Logger logger = LoggerFactory.getLogger(LinkHandler.class);
     private long duplicateTimeout;
     private State lastState;
     private long lastUpdate;
@@ -31,20 +34,26 @@ public class LinkHandler extends UPBBaseHandler {
      *
      * @param thing the thing that should be handled
      */
-    public LinkHandler(Thing thing) {
+    public LinkHandler(Thing thing) 
+    {
         super(thing);
     }
 
     @Override
-    public void initialize() {
+    public void initialize() 
+    {
         super.initialize();
 
         this.duplicateTimeout = ((Number) getConfig().get(UPBBindingConstants.DUPLICATE_TIMEOUT)).longValue();
     }
 
     @Override
-    protected void updateState(ChannelUID channelUID, State state) {
-        if (state.equals(lastState) && (System.currentTimeMillis() - lastUpdate) < duplicateTimeout) {
+    protected void updateState(ChannelUID channelUID, State state) 
+    {
+      try
+      {
+        if (state.equals(lastState) && (System.currentTimeMillis() - lastUpdate) < duplicateTimeout) 
+        {
             return;
         }
 
@@ -52,10 +61,16 @@ public class LinkHandler extends UPBBaseHandler {
         this.lastUpdate = System.currentTimeMillis();
 
         super.updateState(channelUID, state);
+      }
+      catch (Exception e)
+      {
+        logger.error("LinkHandler updateState error : " + e.getMessage());
+      }
     }
 
     @Override
-    protected boolean isLink() {
+    protected boolean isLink() 
+    {
         return true;
     }
 }
