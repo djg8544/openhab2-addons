96ef34ba6f9990654e7e4525fc86381438abba0e
Dustin Gerold <d_gerold_99@yahoo.com>
2020-01-27 10:43:47 -0600

    general exception handling

--- addons/binding/org.openhab.binding.upb/src/main/java/org/openhab/binding/upb/internal/converter/StateConverter.java
+++ addons/binding/org.openhab.binding.upb/src/main/java/org/openhab/binding/upb/internal/converter/StateConverter.java
@@ -14,21 +14,25 @@ import java.util.Map;
 import org.eclipse.smarthome.core.types.State;
 import org.openhab.binding.upb.UPBBindingConstants;
 import org.openhab.binding.upb.internal.UPBMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Converts between a {@link UPBMessage} and a {@link State} for a particular channel.
  *
- * @author Chris Van Orman
+ * @author Chris Van Orman, Dustin Gerold
  * @since 2.2.0
  */
-public class StateConverter {
-
+public class StateConverter 
+{
     private Map<String, StateChannelConverter> converters = new HashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(StateConverter.class);
 
     /**
      * Instantiates a new {@link StateConverter}.
      */
-    public StateConverter() {
+    public StateConverter() 
+    {
         converters.put(UPBBindingConstants.CHANNEL_SWITCH, new SwitchConverter());
         converters.put(UPBBindingConstants.CHANNEL_BRIGHTNESS, new BrightnessConverter());
     }
@@ -40,13 +44,21 @@ public class StateConverter {
      * @param channel the channel to convert for
      * @return a new equivalent {@link State} or null if one does not exist
      */
-    public State convert(UPBMessage message, String channel) {
+    public State convert(UPBMessage message, String channel) 
+    {
+      State state = null;
 
-        State state = null;
-
-        if (converters.containsKey(channel)) {
+      try
+      {
+        if (converters.containsKey(channel)) 
+        {
             state = converters.get(channel).convert(message);
         }
+      }
+      catch (Exception e)
+      {
+        logger.error("StateConverter convert error : " + e.getMessage());
+      }
 
         return state;
     }
